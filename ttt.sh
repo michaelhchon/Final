49e00061445589757230b268ec3828a6fea75f34
Michael <112598328+michaelhchon@users.noreply.github.com>
2022-12-15 17:30:35 -0800

    created var to store rand row and col

--- ttt.sh
+++ ttt.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 # Player 2 script for tic tac toe game
+
+declare -i r
+declare -i c
+
 while true
 do
-  echo $(($RANDOM % 3 + 1))
-  echo $(($RANDOM % 3 + 1))
+  r = $(($RANDOM % 3 + 1))
+  c = $(($RANDOM % 3 + 1))
+  echo "P2 Move: Row $r, Col $c"
   sleep 1
 done
